47cfdc897f11ccb456e5d59cfd2b5752c693c938
Furkan Çat <75113766+furkan-cat@users.noreply.github.com>
2021-09-10 14:32:07 +0300

    Add files via upload
    Add logic of opened detail container

--- src/components/InvoiceList/InvoiceList.js
+++ src/components/InvoiceList/InvoiceList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import useForm from "../../hooks/useForm";
 import db from "../../firebase";
 // import { collection, getDocs } from "firebase/firestore";
@@ -21,6 +21,19 @@ const InvoiceList = ({ setShowModal, totalInvoices, setTotalInvoices }) => {
       setTotalInvoices(arr.length);
     });
   }, []);
+
+  const [isActive, setIsActive] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [id, setId] = useState();
+  const btnRef = useRef();
+  const detailRef = useRef();
+
+  const openHandle = (data) => {
+    btnRef.current.disabled = setIsActive(data.id);
+    setOpen(!open);
+  };
+
+  const closeHandle = (e) => {};
 
   return (
     <div className="invoice-list-container">
@@ -48,47 +61,81 @@ const InvoiceList = ({ setShowModal, totalInvoices, setTotalInvoices }) => {
 
       <div className="list-container">
         {getData.map((data) => (
-          <div
-            key={data.id}
-            className={`list-container__item ${
-              data-key == data.id && open ? "open" : ""
-            }`}
-          >
-            {!open ? (
-              <React.Fragment>
-                <div>{`#${data.id.slice(0, 6)}`}</div>
-                <div>{data.value.cityFrom}</div>
-                <div>{data.value.countryFrom}</div>
+          <div key={data.id} className="list-container__item">
+            <div
+              className={
+                isActive !== data.id
+                  ? "open-main-container"
+                  : "close-main-container"
+              }
+              id={data.id}
+            >
+              <div>{`#${data.id.slice(0, 6)}`}</div>
+              <div>UI/UX</div>
+              <div>{data.value.cityFrom}</div>
+              <div>{data.value.countryFrom}</div>
+              <div className="status-icon">
+                <span className="status-circle"></span>Pending
+              </div>
+              <button
+                className="forward-btn detailed-btn"
+                onClick={() => openHandle(data)}
+                ref={btnRef}
+                disabled={isActive !== data.id && !open ? false : true}
+              >
+                <IoIosArrowForward className="forward-icon" />
+              </button>
+            </div>
+
+            <div
+              className={
+                open && isActive == data.id
+                  ? "detail-open-container"
+                  : "detail-close-container"
+              }
+            >
+              <div className="list-container__item--open__head-item">
+                <div>Status</div>
                 <div className="status-icon">
                   <span className="status-circle"></span>Pending
                 </div>
+                <button>Edit</button>
+                <button>Delete</button>
+                <button>Mark as Paid</button>
                 <button
                   className="forward-btn"
-                  onClick={() => setOpen(!open)}
-                  data-key={data.id}
+                  onClick={openHandle}
                 >
-                  <IoIosArrowForward className="forward-icon" />
+                  <IoIosArrowForward
+                    className="forward-icon"
+                    name="container"
+                  />
                 </button>
-              </React.Fragment>
-            ) : (
-              <div>
+              </div>
+              <div className="list-container__item--open__up-item">
                 <div>{`#${data.id.slice(0, 6)}`}</div>
                 <div>UI/UX</div>
-
-                <div>Adress to</div>
-
-                <div>Invoice Data</div>
-                <div>....</div>
-
-                <div>Bill To</div>
-                <div>...</div>
-
-                <div>Sent To</div>
-                <div>email to...</div>
               </div>
-            )}
+              <div className="list-container__item--open__mid-item">
+                <div>{data.value.cityFrom}</div>
+                <div>{data.value.countryFrom}</div>
+              </div>
+            </div>
           </div>
         ))}
+        {/* // : ( //{" "}
+            <React.Fragment>
+              // <div>{`#${data.id.slice(0, 6)}`}</div>
+              // <div>UI/UX</div>
+              // <div>Adress to</div>
+              // <div>Invoice Data</div>
+              // <div>....</div>
+              // <div>Bill To</div>
+              // <div>...</div>
+              // <div>Sent To</div>
+              // <div>email to...</div>
+              //{" "}
+            </React.Fragment> */}
 
         {/* <div className="list-container__item">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
